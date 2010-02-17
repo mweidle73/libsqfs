ce1c2bf3ed91a92d002fec10df2e3596c11bbdf4
Helge Bahmann <helge.bahmann@secunet.com>
2010-02-17 10:08:46 +0100

    Support writing file inodes
    File inodes along with their data content are now written
    to the image. File data is stored in blocks only (no packing of
    file tails yet), and data blocks are written uncompressed.

---
+++ examples/simple-file.c
@@ -0,0 +1,24 @@
+#include <libsqfs.h>
+
+const char sentence[] = "The quick brown fox jumped over the lazy dog.\n";
+
+int main(void)
+{
+	libsqfs_destination_t dest = libsqfs_destination_create_for_file("/tmp/test.img", 0644);
+	libsqfs_image_t image = libsqfs_image_create(dest);
+	
+	libsqfs_inodeattr_t fileattr = libsqfs_inodeattr_create_simple(image, 1000, 1000, 0644, 0);
+	libsqfs_data_t data = libsqfs_data_create_for_static_buffer(image, sentence, sizeof(sentence)-1);
+	libsqfs_regular_inode_t file = libsqfs_regular_inode_create(image, fileattr, data);
+	
+	libsqfs_inodeattr_t dirattr = libsqfs_inodeattr_create_simple(image, /* uid */ 1000, /* gid */ 1000, 0755, 0);
+	libsqfs_directory_inode_t rootdir = libsqfs_directory_inode_create(image, dirattr);
+	libsqfs_image_set_root(image, rootdir);
+	
+	libsqfs_directory_add_entry(rootdir, "sentence.txt", libsqfs_regular_inode_downcast(file));
+	
+	libsqfs_image_close(image);
+	libsqfs_destination_release(dest);
+	
+	return 0;
+}
---
+++ src/regular.c
@@ -0,0 +1,122 @@
+#include "internal.h"
+
+#include <string.h>
+
+#include "squashfs_fs.h"
+
+struct _libsqfs_regular_inode {
+	LIBSQFS_INODE_COMMON
+	
+	libsqfs_off_t file_size;
+	size_t nblocks;
+	libsqfs_chunk ** blocks;
+};
+
+static size_t
+libsqfs_regular_inode_encoded_size(libsqfs_inode_t inode)
+{
+	libsqfs_regular_inode_t reg = (libsqfs_regular_inode_t) inode;
+	
+	return sizeof(struct squashfs_reg_inode_header) + reg->nblocks * 4;
+}
+
+static void
+libsqfs_regular_inode_encode(libsqfs_inode_t inode, void * dst)
+{
+	libsqfs_regular_inode_t reg = (libsqfs_regular_inode_t) inode;
+	
+	struct squashfs_reg_inode_header * hdr = dst;
+	dst = hdr + 1;
+	
+	hdr->inode_type = cpu_to_le16(SQUASHFS_FILE_TYPE);
+	hdr->mode = cpu_to_le16(reg->attr->mode);
+	hdr->uid = cpu_to_le16(reg->attr->mapped_uid);
+	hdr->guid = cpu_to_le16(reg->attr->mapped_gid);
+	hdr->mtime = cpu_to_le32(reg->attr->ctime);
+	hdr->inode_number = cpu_to_le32(reg->inode_number);
+	
+	/* FIXME: there may not be a first block... */
+	hdr->start_block = cpu_to_le32(reg->blocks[0]->dst.offset);
+	/* FIXME: write fragment info */
+	hdr->fragment = cpu_to_le32(-1);
+	hdr->offset = cpu_to_le32(0);
+	hdr->file_size = cpu_to_le32(reg->file_size);
+	
+	unsigned int * block_info = dst;
+	size_t n;
+	for(n=0; n<reg->nblocks; n++) {
+		uint32_t value = reg->blocks[n]->dst.size;
+		if (!reg->blocks[n]->dst.compressed)
+			value |= SQUASHFS_COMPRESSED_BIT_BLOCK;
+		*block_info = cpu_to_le32(value);
+		block_info++;
+	}
+}
+
+static void
+libsqfs_regular_inode_destroy(libsqfs_inode_t inode)
+{
+	libsqfs_regular_inode_t reg = (libsqfs_regular_inode_t) inode;
+	free(reg->blocks);
+	free(reg);
+}
+
+static const libsqfs_inode_vmt libsqfs_regular_inode_vmt = {
+	.encoded_size = &libsqfs_regular_inode_encoded_size,
+	.encode = &libsqfs_regular_inode_encode,
+	.destroy = &libsqfs_regular_inode_destroy
+};
+
+libsqfs_regular_inode_t
+libsqfs_regular_inode_create(libsqfs_image_t image, libsqfs_inodeattr_t attr, libsqfs_data_t data)
+{
+	libsqfs_off_t file_size = libsqfs_data_get_size(data);
+	/* FIXME: flag error on image */
+	if (file_size==-1) return 0;
+	
+	/* FIXME: this currently stores file tails in blocks as well; this
+	works, but support for coalescing multiple tails in a fragment needs
+	to be implemented as well */
+	size_t nblocks = (file_size + image->block_size-1) / image->block_size;
+	
+	libsqfs_regular_inode_t reg = malloc(sizeof(*reg));
+	if (!reg) return 0;
+	
+	reg->nblocks = nblocks;
+	reg->blocks = malloc(nblocks * sizeof(reg->blocks[0]));
+	if (!reg->blocks) {
+		/* FIXME: flag error on image */
+		free(reg);
+		return 0;
+	}
+	
+	memset(reg->blocks, 0, nblocks * sizeof(reg->blocks[0]));
+	
+	reg->vmt = &libsqfs_regular_inode_vmt;
+	reg->attr = attr;
+	reg->nlink = 0;
+	reg->file_size = file_size;
+	/* FIXME: sometimes need LREG_TYPE */
+	reg->encoded_type = SQUASHFS_FILE_TYPE;
+	libsqfs_inode_init(image, (libsqfs_inode_t) reg);
+	
+	size_t n;
+	for(n=0; n<nblocks; n++) {
+		libsqfs_off_t offset = n * (libsqfs_off_t)image->block_size;
+		size_t size = image->block_size;
+		if (file_size-offset < image->block_size) size = file_size - offset;
+		libsqfs_chunk * chunk = libsqfs_image_submit_chunk_for_data(
+			image, data, offset, size);
+		/* FIXME: flag error on image */
+		if (!chunk) return 0;
+		reg->blocks[n] = chunk;
+	}
+	
+	return reg;
+}
+
+libsqfs_inode_t
+libsqfs_regular_inode_downcast(libsqfs_regular_inode_t inode)
+{
+	return (libsqfs_inode_t) inode;
+}
