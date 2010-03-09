e2e8108ec0a7b9eb8b94d6aefbd86effc8ea7d74
Helge Bahmann <helge.bahmann@secunet.com>
2010-03-09 17:14:48 +0100

    Extended file encoding support
    Support encoding file as "lreg_inode" type, allowing larger
    files, sparse files, recording correct hardlink count and
    extended attributes.

--- src/regular.c
+++ src/regular.c
@@ -1,65 +1,18 @@
 #include "internal.h"
 
 #include <string.h>
+#include <limits.h>
 
 #include "squashfs_fs.h"
 
 struct _libsqfs_regular_inode {
 	LIBSQFS_INODE_COMMON
 	
-	libsqfs_off_t file_size;
+	libsqfs_off_t file_size, sparse_size;
 	size_t nblocks;
 	libsqfs_full_block ** blocks;
 	libsqfs_fragment_piece * tail_piece;
 };
-
-static size_t
-libsqfs_regular_inode_encoded_size(libsqfs_regular_inode_t reg)
-{
-	/* FIXME: sometimes need LREG_TYPE */
-	return sizeof(struct squashfs_reg_inode_header) + reg->nblocks * 4;
-}
-
-static void
-libsqfs_regular_inode_encode(libsqfs_regular_inode_t reg, void * dst)
-{
-	
-	struct squashfs_reg_inode_header * hdr = dst;
-	dst = hdr + 1;
-	
-	/* FIXME: sometimes need LREG_TYPE */
-	reg->encoded_type = SQUASHFS_FILE_TYPE;
-	
-	hdr->inode_type = cpu_to_le16(reg->encoded_type);
-	hdr->mode = cpu_to_le16(reg->attr->mode);
-	hdr->uid = cpu_to_le16(reg->attr->mapped_uid);
-	hdr->guid = cpu_to_le16(reg->attr->mapped_gid);
-	hdr->mtime = cpu_to_le32(reg->attr->ctime);
-	hdr->inode_number = cpu_to_le32(reg->inode_number);
-	
-	if (reg->nblocks)
-		hdr->start_block = cpu_to_le32(reg->blocks[0]->dst.offset);
-	else
-		hdr->start_block = cpu_to_le32(0);
-	if (reg->tail_piece) {
-		hdr->fragment = cpu_to_le32(reg->tail_piece->fragment_block->index);
-		hdr->offset = cpu_to_le32(reg->tail_piece->offset);
-	} else {
-		hdr->fragment = cpu_to_le32(-1);
-		hdr->offset = cpu_to_le32(0);
-	}
-	hdr->file_size = cpu_to_le32(reg->file_size);
-	
-	unsigned int * block_info = dst;
-	size_t n;
-	for(n=0; n<reg->nblocks; n++) {
-		uint32_t value = reg->blocks[n]->dst.size;
-		if (!reg->blocks[n]->dst.compressed)
-			value |= SQUASHFS_COMPRESSED_BIT_BLOCK;
-		*block_info = cpu_to_le32(value);
-		block_info++;
-	}
-}
 
 static void
 libsqfs_regular_inode_destroy(libsqfs_inode_t inode)
@@ -75,12 +28,74 @@ libsqfs_regular_inode_serialize(libsqfs_inode_t inode)
 	libsqfs_regular_inode_t reg = (libsqfs_regular_inode_t) inode;
 	libsqfs_image_t image = reg->image;
 	
-	size_t encoded_size = libsqfs_regular_inode_encoded_size(reg);
-	char buffer[encoded_size];
-	libsqfs_regular_inode_encode(reg, buffer);
+	reg->encoded_type = SQUASHFS_FILE_TYPE;
+	if (reg->sparse_size || reg->nlink != 1 || reg->file_size > UINT_MAX)
+		reg->encoded_type = SQUASHFS_LREG_TYPE;
+	if (reg->nblocks && reg->blocks[0]->dst.offset > UINT_MAX)
+		reg->encoded_type = SQUASHFS_LREG_TYPE;
 	
-	if (!libsqfs_metatable_append(&image->inode_table.tab, buffer, encoded_size, &reg->inode_table_entry)) return false;
-	return true;
+	if (reg->encoded_type == SQUASHFS_FILE_TYPE) {
+		struct squashfs_reg_inode_header hdr;
+		hdr.inode_type = cpu_to_le16(reg->encoded_type);
+		hdr.mode = cpu_to_le16(reg->attr->mode);
+		hdr.uid = cpu_to_le16(reg->attr->mapped_uid);
+		hdr.guid = cpu_to_le16(reg->attr->mapped_gid);
+		hdr.mtime = cpu_to_le32(reg->attr->ctime);
+		hdr.inode_number = cpu_to_le32(reg->inode_number);
+		
+		if (reg->nblocks)
+			hdr.start_block = cpu_to_le32(reg->blocks[0]->dst.offset);
+		else
+			hdr.start_block = cpu_to_le32(0);
+		if (reg->tail_piece) {
+			hdr.fragment = cpu_to_le32(reg->tail_piece->fragment_block->index);
+			hdr.offset = cpu_to_le32(reg->tail_piece->offset);
+		} else {
+			hdr.fragment = cpu_to_le32(-1);
+			hdr.offset = cpu_to_le32(0);
+		}
+		hdr.file_size = cpu_to_le32(reg->file_size);
+		if (!libsqfs_metatable_append(&image->inode_table.tab, &hdr, sizeof(hdr), &reg->inode_table_entry))
+			return false;
+	} else {
+		struct squashfs_lreg_inode_header hdr;
+		hdr.inode_type = cpu_to_le16(reg->encoded_type);
+		hdr.mode = cpu_to_le16(reg->attr->mode);
+		hdr.uid = cpu_to_le16(reg->attr->mapped_uid);
+		hdr.guid = cpu_to_le16(reg->attr->mapped_gid);
+		hdr.mtime = cpu_to_le32(reg->attr->ctime);
+		hdr.inode_number = cpu_to_le32(reg->inode_number);
+		
+		hdr.file_size = cpu_to_le64(reg->file_size);
+		hdr.sparse = cpu_to_le64(reg->sparse_size);
+		hdr.nlink = cpu_to_le32(reg->nlink);
+		if (reg->nblocks)
+			hdr.start_block = cpu_to_le64(reg->blocks[0]->dst.offset);
+		else
+			hdr.start_block = cpu_to_le64(0);
+		if (reg->tail_piece) {
+			hdr.fragment = cpu_to_le32(reg->tail_piece->fragment_block->index);
+			hdr.offset = cpu_to_le32(reg->tail_piece->offset);
+		} else {
+			hdr.fragment = cpu_to_le32(-1);
+			hdr.offset = cpu_to_le32(0);
+		}
+		hdr.xattr = cpu_to_le32(-1);
+		if (!libsqfs_metatable_append(&image->inode_table.tab, &hdr, sizeof(hdr), &reg->inode_table_entry))
+			return false;
+	}
+	
+	unsigned int block_info[reg->nblocks];
+	size_t n;
+	for(n=0; n<reg->nblocks; n++) {
+		uint32_t value = reg->blocks[n]->dst.size;
+		if (!reg->blocks[n]->dst.compressed)
+			value |= SQUASHFS_COMPRESSED_BIT_BLOCK;
+		block_info[n] = cpu_to_le32(value);
+	}
+	
+	libsqfs_metatable_entry dummy;
+	return libsqfs_metatable_append(&image->inode_table.tab, block_info, sizeof(block_info), &dummy);
 }
 
 static const libsqfs_inode_vmt libsqfs_regular_inode_vmt = {
@@ -93,7 +108,7 @@ libsqfs_regular_inode_create(libsqfs_image_t image, libsqfs_inodeattr_t attr, li
 {
 	libsqfs_off_t file_size = libsqfs_data_get_size(data);
 	/* FIXME: flag error on image */
-	if (file_size==-1) return 0;
+	if (file_size == -1) return 0;
 	
 	size_t nblocks, tail_size;
 	
@@ -135,6 +150,7 @@ libsqfs_regular_inode_create(libsqfs_image_t image, libsqfs_inodeattr_t attr, li
 	reg->attr = attr;
 	reg->nlink = 0;
 	reg->file_size = file_size;
+	reg->sparse_size = 0;
 	libsqfs_inode_init(image, (libsqfs_inode_t) reg);
 	
 	size_t n;
