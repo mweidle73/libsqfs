15ad98e3dd1760fdad8aa8ce1992258ae60286c5
Helge Bahmann <helge.bahmann@secunet.com>
2010-03-10 16:05:11 +0100

    Add copyright notices

--- src/threadpool.c
+++ src/threadpool.c
@@ -1,3 +1,22 @@
+/*
+ * Library for creating squashfs filesystem images.
+ *
+ * Copyright (c) 2010
+ * secunet Security Networks AG, Helge Bahmann <helge.bahmann@secunet.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Refer to the file "COPYING" for details.
+ */
+
 #include "internal.h"
 #include <unistd.h>
 #include <signal.h>
